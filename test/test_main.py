33ff7c5c5f00b04508962d68ec617f75bbcc76ed
Javier Garea <javier.garea@udc.es>
2019-07-26 22:37:12 +0200

    FIX test_main. Missing positional arguments.

--- test/test_main.py
+++ test/test_main.py
@@ -8,7 +8,7 @@ OUTPUT_PATH = 'test/g91/output/'
 def test_prog():
     for i in range(1, 7):
         input_path = INPUT_PATH + ('prog0%s.lp' % i)
-        result = {frozenset(model) for model in g91.process(input_path)}
+        result = {frozenset(model) for model in g91.process(0, [input_path])}
         pretty_result = support.formalize(result)
         with open(OUTPUT_PATH + 'sol0%s.txt' % i, 'r') as myfile:
             sol = myfile.read()
