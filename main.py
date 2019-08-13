442b067dbd8647b8282846b2bfb05accd3495178
Javier Garea <javier.garea@udc.es>
2019-08-13 16:31:14 +0200

    It is possible to compute world views under K14 semantics.

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ import src.solver.solver as solver
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-n', '--models', type=int,
-                        help='maximum number of models to compute (0 computes all models)',
-                        default=1)
+                           help='maximum number of models to compute (0 computes all models)',
+                           default=1)
+    argparser.add_argument('-k', '--k14', action='store_true',
+                           help='computes world views under K14 semantics')
     argparser.add_argument('input_files', nargs='+', type=str, help='path to input files')
     args = argparser.parse_args()
 
-    candidates_gen, candidates_test, epistemic_atoms = parser.parse(args.input_files)
+    candidates_gen, candidates_test, epistemic_atoms = parser.parse(args.input_files, args.k14)
 
     start = timer()
 
     for model in solver.solve(candidates_gen, candidates_test, epistemic_atoms, args.models):
-        print([str(atom).replace('aux_', 'K{ ').replace('not_', '~ ').replace('sn_', '-')+' }' for atom in model])
+        print([str(atom).replace('aux_', 'K{ ').replace('not_', '~ ').replace('sn_', '-')+' }'
+               for atom in model])
 
     end = timer()
     print('Elapsed time: %.6f' % (end - start))
