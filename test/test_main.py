3d2b0161b5d287505cfd9cbed4246aa22854fb5f
Javier Garea <javier.garea@udc.es>
2020-01-29 15:04:04 +0100

    ADD updated tests. (OOP)

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,4 @@
-from eclingo.main import Eclingo
-from eclingo.parser.parser import Parser
-from eclingo.solver.solver import Solver
-from eclingo.parser.semantics import EclingoSemantics
+import eclingo.main as eclingo
 
 
 INPUT_PROG_PATH = 'test/prog/input/'
@@ -16,171 +13,86 @@ INPUT_YALE_PATH = 'test/yale/input/'
 OUTPUT_YALE_PATH = 'test/yale/output/'
 
 
-def test_main_prog_g91():
+def test_prog_g91():
     for i in range(1, 8):
-        eclingo_control = Eclingo()
-        input_path = INPUT_PROG_PATH + ('prog%02d.lp' % i)
-        result = [
-            model for model in eclingo_control.main([input_path, '-n', '0'])
-            ]
+        eclingo_control = eclingo.Control(models=0,
+                                          semantics=False,
+                                          optimization=eclingo.__optimization__)
+        input_path = INPUT_PROG_PATH + 'prog{:02d}.lp'.format(i)
+        eclingo_control.load(input_path)
+        eclingo_control.parse()
+        result = [model for model in eclingo_control.solve()]
         result = str(sorted(result)).replace(' ', '')
-        with open(OUTPUT_PROG_PATH + 'sol%02d.txt' % i, 'r') as output_prog:
+        with open(OUTPUT_PROG_PATH + 'sol{:02d}.lp'.format(i), 'r') as output_prog:
             sol = output_prog.read()
             sol = sol.replace('\n', '').replace(' ', '')
         assert result == sol
 
-def test_mod_prog_g91():
+def test_prog_k14():
     for i in range(1, 8):
-        with open(INPUT_PROG_PATH + ('prog%02d.lp' % i), 'r') as input_prog:
-            program = input_prog.read()
-
-        parser = Parser(EclingoSemantics.G91, optimization=1)
-        parser.add(program)
-        gen, test, ep_atoms, show_sign = parser.parse()
-
-        solver = Solver(gen, test, ep_atoms, show_sign, models=0)
-        result = str(sorted([model for model in solver.solve()]))
-        result = result.replace(' ', '')
-        with open(OUTPUT_PROG_PATH + 'sol%02d.txt' % i, 'r') as output_prog:
-            sol = output_prog.read()
-            sol = sol.replace('\n', '').replace(' ', '')
-        assert result == sol
-
-def test_main_prog_k14():
-    for i in range(1, 8):
-        eclingo_control = Eclingo()
-        input_path = INPUT_PROG_PATH + ('prog%02d.lp' % i)
-        result = [
-            model for model in eclingo_control.main([input_path, '-n', '0', '-k'])
-            ]
+        eclingo_control = eclingo.Control(models=0,
+                                          semantics=True,
+                                          optimization=eclingo.__optimization__)
+        input_path = INPUT_PROG_PATH + 'prog{:02d}.lp'.format(i)
+        eclingo_control.load(input_path)
+        eclingo_control.parse()
+        result = [model for model in eclingo_control.solve()]
         result = str(sorted(result)).replace(' ', '')
-        with open(OUTPUT_PROG_PATH + 'sol%02d.txt' % i, 'r') as output_prog:
+        with open(OUTPUT_PROG_PATH + 'sol{:02d}.lp'.format(i), 'r') as output_prog:
             sol = output_prog.read()
             sol = sol.replace('\n', '').replace(' ', '')
         assert result == sol
 
-def test_mod_prog_k14():
-    for i in range(1, 8):
-        with open(INPUT_PROG_PATH + ('prog%02d.lp' % i), 'r') as input_prog:
-            program = input_prog.read()
-
-        parser = Parser(EclingoSemantics.K14, optimization=1)
-        parser.add(program)
-        gen, test, ep_atoms, show_sign = parser.parse()
-
-        solver = Solver(gen, test, ep_atoms, show_sign, models=0)
-        result = str(sorted([model for model in solver.solve()]))
-        result = result.replace(' ', '')
-        with open(OUTPUT_PROG_PATH + 'sol%02d.txt' % i, 'r') as output_prog:
-            sol = output_prog.read()
-            sol = sol.replace('\n', '').replace(' ', '')
-        assert result == sol
-
-def test_main_eligible_g91():
+def test_eligible_g91():
     for i in range(1, 17):
-        eclingo_control = Eclingo()
-        input_path = INPUT_ELIGIBLE_PATH + ('eligible%02d.lp' % i)
-        result = [
-            model for model in eclingo_control.main([KB_ELIGIBLE_PATH, input_path, '-n', '0'])
-            ]
+        eclingo_control = eclingo.Control(models=0,
+                                          semantics=False,
+                                          optimization=eclingo.__optimization__)
+        input_path = INPUT_ELIGIBLE_PATH + 'eligible{:02d}.lp'.format(i)
+        eclingo_control.load(KB_ELIGIBLE_PATH)
+        eclingo_control.load(input_path)
+        eclingo_control.parse()
+        result = [model for model in eclingo_control.solve()]
         result = str(sorted(result)).replace(' ', '')
-        with open(OUTPUT_ELIGIBLE_PATH + 'sol_eligible%02d.txt' % i, 'r') as output_prog:
+        with open(OUTPUT_ELIGIBLE_PATH + 'sol_eligible{:02d}.lp'.format(i), 'r') as output_prog:
             sol = output_prog.read()
             sol = sol.replace('\n', '').replace(' ', '')
         assert result == sol
 
-def test_mod_eligible_g91():
+def test_eligible_k14():
     for i in range(1, 17):
-        with open(KB_ELIGIBLE_PATH, 'r') as eligible_kb:
-            problem_kb = eligible_kb.read()
-        with open(INPUT_ELIGIBLE_PATH + ('eligible%02d.lp' % i), 'r') as input_prog:
-            program = input_prog.read()
-
-        parser = Parser(EclingoSemantics.G91, optimization=1)
-        parser.add(problem_kb+program)
-        gen, test, ep_atoms, show_sign = parser.parse()
-
-        solver = Solver(gen, test, ep_atoms, show_sign, models=0)
-        result = str(sorted([model for model in solver.solve()]))
-        result = result.replace(' ', '')
-        with open(OUTPUT_ELIGIBLE_PATH + 'sol_eligible%02d.txt' % i, 'r') as output_prog:
-            sol = output_prog.read()
-            sol = sol.replace('\n', '').replace(' ', '')
-        assert result == sol
-
-def test_main_eligible_k14():
-    for i in range(1, 17):
-        eclingo_control = Eclingo()
-        input_path = INPUT_ELIGIBLE_PATH + ('eligible%02d.lp' % i)
-        result = [
-            model for model in eclingo_control.main([KB_ELIGIBLE_PATH, input_path, '-n', '0', '-k'])
-            ]
+        eclingo_control = eclingo.Control(models=0,
+                                          semantics=True,
+                                          optimization=eclingo.__optimization__)
+        input_path = INPUT_ELIGIBLE_PATH + 'eligible{:02d}.lp'.format(i)
+        eclingo_control.load(KB_ELIGIBLE_PATH)
+        eclingo_control.load(input_path)
+        eclingo_control.parse()
+        result = [model for model in eclingo_control.solve()]
         result = str(sorted(result)).replace(' ', '')
-        with open(OUTPUT_ELIGIBLE_PATH + 'sol_eligible%02d.txt' % i, 'r') as output_prog:
+        with open(OUTPUT_ELIGIBLE_PATH + 'sol_eligible{:02d}.lp'.format(i), 'r') as output_prog:
             sol = output_prog.read()
             sol = sol.replace('\n', '').replace(' ', '')
         assert result == sol
 
-def test_mod_eligible_k14():
-    for i in range(1, 17):
-        with open(KB_ELIGIBLE_PATH, 'r') as eligible_kb:
-            problem_kb = eligible_kb.read()
-        with open(INPUT_ELIGIBLE_PATH + ('eligible%02d.lp' % i), 'r') as input_prog:
-            program = input_prog.read()
-
-        parser = Parser(EclingoSemantics.K14, optimization=1)
-        parser.add(problem_kb+program)
-        gen, test, ep_atoms, show_sign = parser.parse()
-
-        solver = Solver(gen, test, ep_atoms, show_sign, models=0)
-        result = str(sorted([model for model in solver.solve()]))
-        result = result.replace(' ', '')
-        with open(OUTPUT_ELIGIBLE_PATH + 'sol_eligible%02d.txt' % i, 'r') as output_prog:
-            sol = output_prog.read()
-            sol = sol.replace('\n', '').replace(' ', '')
-        assert result == sol
-
-def test_main_yale_g91():
+def test_yale_g91():
     for i in range(1, 9):
         if i != 7:
             length = i
             if i == 6:
                 length += 1
 
-            eclingo_control = Eclingo()
-            input_path = INPUT_YALE_PATH + ('yale%02d.lp' % i)
-            result = [
-                model for model in eclingo_control.main([
-                    KB_YALE_PATH, input_path, '-n', '0', '-c', 'length={}'.format(length)
-                    ])
-                ]
+            eclingo_control = eclingo.Control(models=0,
+                                              semantics=True,
+                                              optimization=eclingo.__optimization__)
+            input_path = INPUT_YALE_PATH + 'yale{:02d}.lp'.format(i)
+            eclingo_control.load(KB_YALE_PATH)
+            eclingo_control.load(input_path)
+            eclingo_control.add_const('length', str(length))
+            eclingo_control.parse()
+            result = [model for model in eclingo_control.solve()]
             result = str(sorted(result)).replace(' ', '')
-            with open(OUTPUT_YALE_PATH + 'sol_yale%02d.txt' % i, 'r') as output_prog:
-                sol = output_prog.read()
-                sol = sol.replace('\n', '').replace(' ', '')
-            assert result == sol
-
-def test_mod_yale_g91():
-    for i in range(1, 8):
-        if i != 7:
-            length = i
-            if i == 6:
-                length += 1
-
-            with open(KB_YALE_PATH, 'r') as yale_kb:
-                problem_kb = yale_kb.read()
-            with open(INPUT_YALE_PATH + ('yale%02d.lp' % i), 'r') as input_prog:
-                program = input_prog.read()
-
-            parser = Parser(EclingoSemantics.G91, optimization=1)
-            parser.add(problem_kb+program)
-            parser.add_constant('length')
-            gen, test, ep_atoms, show_sign = parser.parse()
-
-            solver = Solver(gen, test, ep_atoms, show_sign, models=0)
-            result = str(sorted([model for model in solver.solve()]))
-            result = result.replace(' ', '')
-            with open(OUTPUT_YALE_PATH + 'sol_yale%02d.txt' % i, 'r') as output_prog:
+            with open(OUTPUT_YALE_PATH + 'sol_yale{:02d}.lp'.format(i), 'r') as output_prog:
                 sol = output_prog.read()
                 sol = sol.replace('\n', '').replace(' ', '')
             assert result == sol
