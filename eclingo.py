a559ad82c6c0cc9db75f30d7384b1421f38cb632
Javier Garea <javier.garea@udc.es>
2020-01-29 15:08:07 +0100

    ADD new eclingo script + eclingo.Control API

--- eclingo.py
+++ eclingo.py
@@ -1,20 +1,46 @@
-import sys
-from eclingo.main import Eclingo
-
+import argparse
+from timeit import default_timer as timer
+import eclingo.main as eclingo
 
 if __name__ == "__main__":
-    eclingo_control = Eclingo()
+    print('eclingo version {}'.format(eclingo.__version__))
 
-    eclingo_control.print_version()
-    models = 0
-    for model in eclingo_control.main(sys.argv[1:]):
-        models += 1
-        answer = ('\t').join(['&k{ '+str(atom)+' }' for atom in model if 'aux_' in atom.name])
-        answer = answer.replace('aux_', '').replace('not_', '~ ').replace('sn_', '-')
-        print('Answer: %s\n%s' % (models, answer))
+    argparser = argparse.ArgumentParser(prog='eclingo')
+    argparser.add_argument('-n', '--models', type=int,
+                           help='maximum number of models to compute (0 computes all models)',
+                           default=1)
+    argparser.add_argument('-k', '--k14', action='store_true',
+                           help='computes world views under K14 semantics')
+    argparser.add_argument('-op', '--optimization', type=int,
+                           help='number of optimization to use (0 for no optimizations)',
+                           default=eclingo.__optimization__)
+    argparser.add_argument('-c', '--const', action='append',
+                           help='adds a constant to the program (using \'<name>=<term>\' format)')
+    argparser.add_argument('input_files', nargs='+', type=str, help='path to input files')
+    args = argparser.parse_args()
 
-    if models:
+    start = timer()
+
+    eclingo_control = eclingo.Control(models=args.models,
+                                      semantics=args.k14,
+                                      optimization=args.optimization)
+
+    for file_path in args.input_files:
+        eclingo_control.load(file_path)
+    if args.const:
+        for constant in args.const:
+            name, term = constant.split('=')
+            eclingo_control.add_const(name, term)
+
+    eclingo_control.parse()
+    for model in eclingo_control.solve():
+        print('Answer: {models}\n{model}'.format(models=eclingo_control.models, model=model))
+
+    end = timer()
+
+    if eclingo_control.models:
         print('SATISFIABLE\n')
     else:
         print('UNSATISFIABLE\n')
-    print('Elapsed time: %.6f s' % eclingo_control.elapsed_time)
+
+    print('Elapsed time: {:.6f} s'.format(end - start))
--- eclingo/main.py
+++ eclingo/main.py
@@ -1,53 +1,24 @@
-import argparse
-from timeit import default_timer as timer
-import eclingo.parser.parser as parser
-import eclingo.solver.solver as solver
-
-
 __version__ = '1.0.0'
+__optimization__ = 1
 
 
-class Eclingo:
+class Control:
 
-    def __init__(self):
-        self.elapsed_time = None
-        self.version = __version__
-        self.argparser = argparse.ArgumentParser(prog='eclingo')
-        self._set_argparser_args()
+    def __init__(self, models, semantics, optimization):
+        self.models=0
+        pass
 
-    def _set_argparser_args(self):
-        self.argparser.add_argument('-n', '--models', type=int,
-                                    help='maximum number of models to compute ' +
-                                    '(0 computes all models)',
-                                    default=1)
-        self.argparser.add_argument('-k', '--k14', action='store_true',
-                                    help='computes world views under K14 semantics')
-        self.argparser.add_argument('-op', '--optimization', type=int,
-                                    help='number of optimization to use ' +
-                                    '(0 for no optimizations)',
-                                    default=1)
-        self.argparser.add_argument('-c', '--const', action='append',
-                                    help='adds a constant to the program ' +
-                                    '(using \'<id>=<term>\' format)')
-        self.argparser.add_argument('input_files', nargs='+', type=str, help='path to input files')
+    def add(self, name, parameters, program):
+        pass
 
-    def print_version(self):
-        print('eclingo version {}'.format(self.version))
+    def add_const(self, name, value):
+        pass
 
-    def main(self, args):
-        eclingo_args = self.argparser.parse_args(args)
+    def load(self, input_path):
+        pass
 
-        start = timer()
+    def parse(self):
+        pass
 
-        candidates_gen, candidates_test, \
-            epistemic_atoms, show_signatures = parser.parse(eclingo_args.input_files,
-                                                            eclingo_args.const,
-                                                            eclingo_args.k14,
-                                                            eclingo_args.optimization)
-
-        yield from solver.solve(candidates_gen, candidates_test,
-                                epistemic_atoms, show_signatures, eclingo_args.models)
-
-        end = timer()
-
-        self.elapsed_time = end - start
+    def solve(self, _yield=False):
+        pass
