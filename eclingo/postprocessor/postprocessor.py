b11413bf4c6e870e0539db9ab76e02b9673a1f34
Javier Garea <javier.garea@udc.es>
2020-01-31 13:32:16 +0100

    ADD preprocessor + postprocessor modules

---
+++ eclingo/postprocessor/postprocessor.py
@@ -0,0 +1,8 @@
+class Postprocessor:
+
+    def __init__(self, candidates_test, show_signatures):
+        self._candidates_test = candidates_test
+        self._show_signatures = show_signatures
+
+    def postprocess(self, program):
+        pass
---
+++ eclingo/preprocessor/preprocessor.py
@@ -0,0 +1,22 @@
+class G91Preprocessor:
+
+    def __init__(self, candidates_gen, candidates_test):
+        self._candidates_gen = candidates_gen
+        self._candidates_test = candidates_test
+        self.predicates = set()
+        self.show_signatures = set()
+
+    def preprocess(self, program):
+        pass
+
+
+class K14Preprocessor:
+
+    def __init__(self, candidates_gen, candidates_test):
+        self._candidates_gen = candidates_gen
+        self._candidates_test = candidates_test
+        self.predicates = set()
+        self.show_signatures = set()
+
+    def preprocess(self, program):
+        pass
